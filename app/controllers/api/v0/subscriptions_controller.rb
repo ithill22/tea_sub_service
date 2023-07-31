8328993666e5a2333ad8b1679d6722338be545ab
Isaac Thill <ithill22@turing.edu>
2023-07-31 11:31:37 -0600

    add destroy route and action

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -4,6 +4,12 @@ class Api::V0::SubscriptionsController < ApplicationController
     render json: SubscriptionSerializer.new(subscription), status: :created
   end
 
+  def destroy
+    subscription = Subscription.find(params[:id])
+    subscription.update(status: 'cancelled')
+    render json: SubscriptionSerializer.new(subscription), status: :ok
+  end
+
   private
 
   def subscription_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :customers, only: %i[] do
-        resources :subscriptions, only: %i[create]
+        resources :subscriptions, only: %i[create destroy]
       end
     end
   end
