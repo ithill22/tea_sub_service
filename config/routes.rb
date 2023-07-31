91bb42bc03399928b617a293d6a244f9085e07e7
Isaac Thill <ithill22@turing.edu>
2023-07-31 10:56:04 -0600

    add create route, controller with create action and subscription serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v0 do
+      resources :customers, only: %i[] do
+        resources :subscriptions, only: %i[create]
+      end
+    end
+  end
 end
